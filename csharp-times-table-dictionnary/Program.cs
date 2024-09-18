5eacfc4ffd031c68712ecb0004049730aab62222
Onur45500 <onur.akmese@viacesi.fr>
2024-09-18 16:21:20 +0200

    Initialization of the project

--- csharp-times-table-dictionnary/Program.cs
+++ csharp-times-table-dictionnary/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TimesTable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> table = new Dictionary<int, int>();
+
+            string input;
+
+            Console.WriteLine("Write a number to multiply : ");
+            input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            Console.WriteLine("Until what number do you want the multiplication :");
+            input = Console.ReadLine();
+            int secondNumber = Convert.ToInt32(input);
+
+            table.Add(number, secondNumber);
+
+            for(int i = 0; i < table.Count; i++)
+            {
+                var pair = table.ElementAt(i);
+                for(int y = 0; y <= pair.Value; y++)
+                {
+                    Console.WriteLine($"{pair.Key} x {y} = {pair.Key * y}\n");
+                }
+            }
+            
+        }
+    }
+}
